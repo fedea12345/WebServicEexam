8ee68a744c4c597518a1cc894a64fd919d71512b
Mauro <Mauro88rc@gmail.com>
2016-05-24 20:56:40 -0300

    Intento de webService

---
+++ Dalc/DalcProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Dalc
+{
+    public class DalcProduct
+    {
+        public static List<Product> GetProducts()
+        {
+            var context = new NorthwindEntities();
+            return context.Products.ToList();
+        }
+    }
+}
---
+++ WebSHost/IProductService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Entities;
+using Dalc;
+
+namespace WebSHost
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProductService" in both code and config file together.
+    [ServiceContract]
+    public class IProductService : IService1
+    {       
+      public List<ProductDTO> GetListaProduct()
+        {
+            
+            List<Product> produc = new List<Product>();
+            produc = DalcProduct.GetProducts();
+
+
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product,ProductDTO >());
+            var mapper = config.CreateMapper();
+
+            List<ProductDTO> proDTO = mapper.Map<List<ProductDTO>>(produc);
+            return proDTO;
+
+        }
+     
+    }
+    [DataContract]
+    public class ProductDTO
+    {
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public decimal? UnitPrice { get; set; }
+    }
+}
